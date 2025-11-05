7b06a1456fd02ce34db4480781595259c0ad7693
Derek Scruggs <me@derekscruggs.com>
2025-11-05 11:59:50 -0600

    fixes to tailwind

--- projects/exod-tools-frontend/vite.config.ts
+++ projects/exod-tools-frontend/vite.config.ts
@@ -1,15 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     nodePolyfills({
       globals: {
         Buffer: true,
       },
     }),
   ],
-})
+});
